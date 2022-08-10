1e4f8409dce478ecb62dd8fa871fc5c6b26da82b
m-shivamjha <54800520+m-shivamjha@users.noreply.github.com>
2022-08-10 12:07:04 +0530

    Create Week4_1 Longest Palindromic Substring.py

---
+++ Week4_1 Longest Palindromic Substring.py
@@ -0,0 +1,13 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        
+        dp = [[False]*len(s) for _ in range(len(s))]
+        longest = ''
+        
+        for i in range(len(s)):
+            for j in range(i+1):
+                if s[i] == s[j] and ((i+1-j) <= 3 or dp[i-1][j+1]):
+                    dp[i][j] = True
+                    if i+1-j > len(longest):
+                        longest = s[j:i+1]
+        return longest
